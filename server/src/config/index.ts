c75b9651e1fc324506cd1287d4a995cbe087b19c
Andrzej Chuchra <andrzej.chuchra@gmail.com>
2020-11-29 04:42:35 +0100

    added client react builded app

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,13 +2,15 @@ import { Config as Conf } from './development';
 import { ProdConfig } from './production';
 
 interface Config {
-	MONGO_URI: string;
-	JWT_KEY: string;
+  MONGO_URI: string;
+  JWT_KEY: string;
 }
 let Settings: Config = Conf;
 
+console.log('process', process);
+
 if (process.env.NODE_ENV === 'production') {
-	Settings = ProdConfig;
+  Settings = ProdConfig;
 }
 
 export { Settings };
